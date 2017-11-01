85b73c80340e5a32bdbc0b973ba4fdd55bbf454d
Sanchay Javeria <javeria2@illinois.edu>
2017-11-01 01:50:14 -0500

    init

---
+++ remote-update-all.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for (( i = 1; i < 11; i++ )); do
+    var=$(printf "weiren2@fa17-cs425-g28-%02d.cs.illinois.edu" $i)
+    ssh $var 'export PATH=/usr/local/go/bin:$PATH; cd ~/go/src/cs425_mp2 ; git fetch; git checkout false-positive; git pull; go version; go get; go build' &
+done
+wait
