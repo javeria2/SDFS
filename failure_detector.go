3bb79b3989cbc41effe2fc2cd387a73b6bc1566f
Sanchay Javeria <javeria2@illinois.edu>
2017-11-02 02:44:28 -0500

    send files as byte[] array to replicas

--- failure_detector.go
+++ failure_detector.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"os"
 	"io"
+	"io/ioutil"
 	"bufio"
 	"strconv"
 	"strings"
@@ -465,11 +466,6 @@ func tempTest() {
 }
 
 func initialize() {
-	/* sdfs */
-	if vmID == primaryMaster || vmID == secondaryMaster || vmID == thirdMaster {
-	    isMaster = true
-	}
-
 	// Initialize membership list
 	myHeartbeat = 0
 	membershipList = make([]*member, listLength, listLength)
@@ -568,17 +564,9 @@ const (
 )
 
 var (
-	isMaster   = false
   fileMap    = make(map[string][]uint32)
 	sdfsPacket = &heartbeat.SdfsPacket{Source: uint32(vmID)}
 )
-
-/**
-  File op: STORE, Prints the files on the current node.
-*/
-func store() {
-
-}
 
 /**
  File op: PUT, put a local file with filename @localFileName into sdfs with file name @sdfsFileName
@@ -588,20 +576,17 @@ func putFile(localFileName string, sdfsFileName string) {
 		updateFileMap(sdfsFileName, uint32(vmID))
 	} else {
 		// not main master, send msg to master and add files into filemap
-		sendSDFSMessage(primaryMaster, "add", sdfsFileName, vmID)
-	}
-	var firstPeer = vmID + 1
-	var secondPeer = vmID + 2
-	if secondPeer > 10 {
-		secondPeer = 1
-	}
-	if firstPeer > 10 {
-		firstPeer = 1
-		secondPeer = 2
+		sendSDFSMessage(primaryMaster, "add", sdfsFileName, vmID, nil)
 	}
 	makeLocalReplicate(sdfsFileName, localFileName)
-	replicate(sdfsFileName, firstPeer)
-	replicate(sdfsFileName, secondPeer)
+	replicate(sdfsFileName, vmID)
+}
+
+/**
+  File op: STORE, Prints the files on the current node.
+*/
+func store() {
+
 }
 
 /**
@@ -672,14 +657,32 @@ func makeLocalReplicate(sdfsFileName string, localFileName string) {
   out.Sync()
 }
 
+/**
+	Replicate the file with name sdfsFileName on nodeID + 1, nodeID + 2.
+*/
 func replicate(sdfsFileName string, nodeID int) {
-
+	var firstPeer = nodeID + 1
+	var secondPeer = nodeID + 2
+	if secondPeer > 10 {
+		secondPeer = 1
+	}
+	if firstPeer > 10 {
+		firstPeer = 1
+		secondPeer = 2
+	}
+	fi, err := ioutil.ReadFile(sdfsFileName)
+	if err != nil {
+		fmt.Printf("error %s\n", err)
+		return
+	}
+	sendSDFSMessage(firstPeer, "file", sdfsFileName, vmID, fi)
+	sendSDFSMessage(secondPeer, "file", sdfsFileName, vmID, fi)
 }
 
 /**
 	Send an sdfs message packet to node with id @nodeID
 */
-func sendSDFSMessage(nodeID int, message string, sdfsFileName string, vmID int) {
+func sendSDFSMessage(nodeID int, message string, sdfsFileName string, vmID int, file []byte) {
 	if iHaveLeft {
 		// Do nothing if the node has left
 		time.Sleep(time.Nanosecond)
@@ -689,17 +692,20 @@ func sendSDFSMessage(nodeID int, message string, sdfsFileName string, vmID int) 
 	// construct our msg
 	sdfsPacket.Msg = message
 	sdfsPacket.SdfsFileName = sdfsFileName
+	if file != nil {
+		sdfsPacket.File = file
+	}
 
 	//Marshal the msg
 	m, err := proto.Marshal(sdfsPacket)
 	if err != nil {
-		fmt.Printf("error has occured! %s\n", err)
+		fmt.Printf("error %s\n", err)
 		return
 	}
 
 	conn, err := net.Dial("udp", fmt.Sprintf(nodeName, nodeID, sdfsPort))
 	if err != nil {
-		fmt.Printf("error has occured! %s\n", err)
+		fmt.Printf("error %s\n", err)
 		return
 	}
 	//defer close and write message to tcp connection
@@ -714,8 +720,8 @@ func receiveSDFSMessage() {
 	//set up tcp listener
 	conn, err := net.ListenPacket("udp", sdfsPort)
 	if err != nil {
-		fmt.Printf("error has occured! %s\n", err)
-		myLog.Fatal(err)
+		fmt.Printf("error %s\n", err)
+		return
 	}
 	defer conn.Close()
 
@@ -730,22 +736,31 @@ func receiveSDFSMessage() {
 		// continue listenning
 		n, addr, err := conn.ReadFrom(buf)
 		if err != nil {
-			fmt.Println("Error: ", err)
-			myLog.Fatal(err)
+			fmt.Println("error: ", err)
+			return
 		}
 		sdfsMsg := &heartbeat.SdfsPacket{}
 		if err := proto.Unmarshal(buf[0:n], sdfsMsg); err != nil {
 			fmt.Printf("Failed. Error: %s\n", err)
-			myLog.Fatal(err)
 			return
 		}
 		// fmt.Println("n: ", n)
 		// fmt.Println(proto.MarshalTextString(sdfsMsg))
 		myLog.Printf("Message sent from node %d (IP: %s).\n", sdfsMsg.GetSource(), addr.String())
-		if sdfsMsg.GetMsg() == "add" {
-			updateFileMap(sdfsMsg.GetSdfsFileName(), sdfsMsg.GetSource())
+		switch sdfsMsg.GetMsg() {
+			case "add":
+				updateFileMap(sdfsMsg.GetSdfsFileName(), sdfsMsg.GetSource())
+			case "file":
+				saveFile(sdfsMsg.GetSdfsFileName(), sdfsMsg.GetFile())
 		}
 	}
+}
+
+/**
+	Save file @file locally with name @sdfsFileName
+*/
+func saveFile(sdfsFileName string, file []byte) {
+
 }
 
 /**
